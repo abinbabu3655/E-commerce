d8703850566a8aab8c0c406d6264b3186fd450c7
abinbabu3655 <abinbabu535@gmail.com>
2022-08-04 16:29:22 +0530

    v1.5

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const state = {
 }
 
 module.exports.connect = (done) => {
-    const url = 'mongodb://localhost:27017'
+    const url = 'mongodb+srv://abin:Abin@3043@cluster.8dx1hde.mongodb.net/?retryWrites=true&w=majority'
     const dbname = 'shopping'
 
     mongoClient.connect(url, (err, data) => {
